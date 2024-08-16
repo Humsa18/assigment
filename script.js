19d7181d7caabf4cac932cc86c487e8b31380110
syedahoor <syedahumsagardazee@gmail.com>
2024-08-16 15:26:23 +0500

    my assigment

---
+++ script.js
@@ -0,0 +1,66 @@
+
+let text1 = [heelo,cup,chiye,me];
+text1.toString();
+console.log(text1);
+
+
+
+
+
+
+
+
+
+
+
+
+let silly =[hello,world,mine, here]
+silly.shift();
+console.log(silly);
+
+
+
+
+
+let text = [one ,two, three, four,five];
+text.push("six")
+console.log(text);
+
+
+
+let fruite=[apple, mango, orange,bannan]
+fruite.pop();
+console.log(fruite);
+
+
+let name =[hello,world,sea,food];
+name.unshift(hello,food)
+console.log(name);
+
+
+
+let upper = [Hello,World,bye,world];
+upper.join();
+console.log(upper);
+
+
+let copy = [How,are,you,doing,today];
+copy.delete(you);
+console.log(copy);
+
+let world = [Hello,world,bye,world];
+let world2 =[see,food,water,dri];
+let world3 =world.concat(world2);
+console.log(world3);
+
+let hello = [Hello,world,bye,mrchips];
+hello.reverse();
+console.log(hello);
+
+let position = [Mr,Blue,has,a,blue,house];
+position.sort();
+console.log(position);
+
+let cast = [i,belong,to,syed,family]
+position.slice(endstart);
+console.log(position);
